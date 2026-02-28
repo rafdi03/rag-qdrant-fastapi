33b39721f2705a9d191d6c69c79d0e798ca6f935
rafdi -5 <141027900+rafdi03@users.noreply.github.com>
2026-02-28 17:23:43 +0700

    Update setting.py

--- app/setting.py
+++ app/setting.py
@@ -6,6 +6,6 @@ class Settings(BaseSettings):
     EMBEDDING_MODEL: str = "sentence-transformers/all-MiniLM-L6-v2"
 
     OLLAMA_URL: str = "http://localhost:11434"
-    OLLAMA_MODEL: str = "tinyllama"  # atau "mistral"
+    OLLAMA_MODEL: str = "tinyllama" 
 
 settings = Settings()
